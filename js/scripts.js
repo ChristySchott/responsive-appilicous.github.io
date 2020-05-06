a114cb8e6b71f1c75cb98c84c06e213282c2b00c
Christy Hauschild Schott <christyhauschild@gmail.com>
2020-05-06 09:48:34 -0300

    General updates

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.nav-toggle').click(function () {
+        $('.main-nav').toggleClass('is-open');
+        $('.hamburger').toggleClass('is-open');
+    });
+});
